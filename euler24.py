56d7bd919b0977cfb6e555d24085c181c363cdc2
epistola <evangelos.pistolas@gmail.com>
2017-08-25 11:33:49 +0300

    euler24.py added

--- euler24.py
+++ euler24.py
@@ -1,1 +1,8 @@
-from itertools import permutations, combinations
+from itertools import permutations, combinations, islice
+
+def compute():
+    ans = next(islice(sorted(permutations(range(10))), 999999, None), None)
+    return str(ans)
+
+if __name__ == '__main__':
+    print(compute())
